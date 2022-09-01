a8fa0e7c594ed3834905c88e76dc32906dc3e0e9
ZJ <zijianchua0@gmail.com>
2022-09-01 14:14:37 +0800

    bluetooth added

--- app/src/main/java/com/example/cz3004_android/MainActivity.java
+++ app/src/main/java/com/example/cz3004_android/MainActivity.java
@@ -1,14 +1,121 @@
 package com.example.cz3004_android;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.cz3004_android.Bluetooth.BluetoothPairingActivity;
+import com.example.cz3004_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private Context context;
+    private BluetoothAdapter bluetoothAdapter ;
 
+    private final int LOCATION_PERMISSION_REQUEST = 101;
+    private final int SELECT_DEVICE = 102;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
+        replaceFragment(new RobotControlFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item->{
+            switch(item.getItemId()){
+                case R.id.nav_robot_cntrl:
+                    replaceFragment(new RobotControlFragment());
+                    break;
+                case R.id.nav_bt_msg:
+                    replaceFragment(new BTMessageFragment());
+                    break;
+
+            }
+            return true;
+        });
+    }
+    private void init(){
+        context=this;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.secure_connect_scan:
+                checkPermissions();
+                break;
+            case R.id.discoverable:
+                ensureDiscoverable();
+                break;
+            default:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent(context, BluetoothPairingActivity.class);
+            startActivityForResult(intent, SELECT_DEVICE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, BluetoothPairingActivity.class);
+                startActivityForResult(intent, SELECT_DEVICE);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Location permission is required.\n Please grant")
+                        .setPositiveButton("Grant", (dialogInterface, i) -> checkPermissions())
+                        .setNegativeButton("Deny", (dialogInterface, i) -> MainActivity.this.finish()).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
+     * Makes this device discoverable for 300 seconds (5 minutes).
+     */
+    @SuppressLint("MissingPermission")
+    private void ensureDiscoverable() {
+        if (bluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
     }
 }
